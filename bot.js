403ea9b3c870d535d81d0d8d03752de0788d95b1
iDragonize120 <55050392+iDragonize120@users.noreply.github.com>
2019-09-08 13:40:11 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("620215439951396881")
+setInterval(function() {
+channel.send(`سباااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااااام`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
